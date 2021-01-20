3f226c1af5f8663e9ffa7dff0d10c1e405760067
unitedProgrammer <77531232+unitedProgrammer@users.noreply.github.com>
2021-01-20 22:09:38 +0200

    node js

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const exphbs = require("express-handlebars");
+const todoRoutes = require("./routes/todos");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+const hbs = exphbs.create({
+  defaultLayout: "main",
+  extname: "hbs",
+});
+
+app.engine("hbs", hbs.engine);
+app.set("view engine", "hbs");
+app.set("views", "views");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(todoRoutes);
+
+async function start() {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://dimon:09220927@cluster0.jledq.mongodb.net/todos?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useFindAndModify: false,
+      }
+    );
+    app.listen(PORT, () => {
+      console.log("server has been started");
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+start();
---
+++ routes/todos.js
@@ -0,0 +1,28 @@
+const { Router } = require("express");
+const Todo = require("../models/Todo");
+const router = Router();
+
+router.get("/", async (req, res) => {
+  const todos = await Todo.find({});
+  res.render("index", {
+    title: "Todos list",
+    isIndex: true,
+    todos,
+  });
+});
+
+router.get("/create", (req, res) => {
+  res.render("create", {
+    title: "create todo",
+    isCreate: true,
+  });
+});
+
+router.post("/create", async (req, res) => {
+  const todo = new Todo({
+    title: req.body.title,
+  });
+  await todo.save();
+  res.redirect("/");
+});
+module.exports = router;
